06aeb55d6194d5c458f9f83783b650eb8d2ddcaa
heyitsjearbear <jereberries1215@gmail.com>
2023-11-25 13:21:25 -0800

    added hover effect, and click to enlarge

--- src/components/TeaCategory/TeaCategory.tsx
+++ src/components/TeaCategory/TeaCategory.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './TeaCategory.css'; // Import the CSS for the tea category
+import React, { useState } from "react";
+import "./TeaCategory.css"; // Import the CSS for the tea category
 
 interface TeaCategoryProps {
   name: string;
@@ -7,15 +7,43 @@ interface TeaCategoryProps {
   imageUrl: string;
 }
 
-const TeaCategory: React.FC<TeaCategoryProps> = ({ name, description, imageUrl }) => {
+const TeaCategory: React.FC<TeaCategoryProps> = ({
+  name,
+  description,
+  imageUrl,
+}) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(true);
+  };
+
+  const handleClose = () => {
+    setIsClicked(false);
+  };
+
   return (
-    <div className="tea-category">
-      <img src={imageUrl} alt={name} />
-      <div className="tea-category-info">
-        <h3>{name}</h3>
-        <p>{description}</p>
+    <>
+      <div className="tea-category" onClick={handleClick}>
+        <img src={imageUrl} alt={name} />
+        <div className="tea-category-info">
+          <h3>{name}</h3>
+          <p>{description}</p>
+        </div>
       </div>
-    </div>
+      {isClicked && (
+        <div className="focused-card">
+          <button onClick={handleClose}>X</button>
+          <div className="tea-category">
+            <img src={imageUrl} alt={name} />
+            <div className="tea-category-info">
+              <h3>{name}</h3>
+              <p>{description}</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
